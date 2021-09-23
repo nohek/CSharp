fef9c9c80318d3dd124b50ae8ba900826b6d2e4c
Francyelle <francyellem64@gmai.com>
2021-09-23 10:43:12 -0300

    getting the day of week you were born

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Globalization;
 
-namespace CSharp
+namespace Methods
 {
-    class Program
+    class BirthDayCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            DateOfBirth();
+            var usersDateOfBirth = AskForDateOfBirth();
+            var dayOfWeek = usersDateOfBirth.DayOfWeek;
+            Console.WriteLine(dayOfWeek);
+
+        }
+
+        public static void DateOfBirth()
+        {
+            Console.WriteLine("i will guess the day of the week you were born on");
+        }
+        public static DateTimeOffset AskForDateOfBirth()
+        {
+            Console.WriteLine("whats your date of birth? " + CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern);
+
+            var userText = Console.ReadLine();
+
+            return DateTimeOffset.Parse(userText);
+
         }
     }
 }
